d4e39234faa5f69eb4fc4abce31f760320fa4f1b
beomgeun <ggoon98@gmail.com>
2024-02-03 17:01:16 +0900

    h2 DB 연동

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,13 +1,19 @@
 package hello.hellospring.service;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean // Bean으로 등록 되어야 AutoWired 적용된다 !!
     public MemberService memberService() {
@@ -15,6 +21,7 @@ public class SpringConfig {
     }
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
